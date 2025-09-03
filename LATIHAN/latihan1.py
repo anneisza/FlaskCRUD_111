1a72612a34960a532d565bab07de66e572293968
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-09-03 12:22:24 +0700

    app

---
+++ LATIHAN/latihan1.py
@@ -0,0 +1,11 @@
+#buatlah aplikasi Flask sederhana yang menampilkan "Hello, World!" di jalan utama!
+from flask import Flask
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
